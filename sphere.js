1878a4af623f8020916e74b6f861ef1681083382
nmudur <nmudur@gmail.com>
2022-06-09 15:46:57 -0400

    Updates

--- sphere.js
+++ sphere.js
@@ -27,7 +27,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
 console.log(sphere)
 scene.add(sphere)
 camera.position.z = 20
-
+renderer.render(scene, camera)
 const group = new THREE.Group()
 group.add(sphere)
 scene.add(group)
